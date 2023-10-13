c5f74bd93896131e0721151c2dc08bdc1bd62a47
flyingSpagetiMonstr <2021302181087@whu.edu.cn>
2023-10-13 14:37:20 +0800

    Disk/read root of file directory, fin.

--- disk/disk.c
+++ disk/disk.c
@@ -3,6 +3,9 @@
 #include <Windows.h>
 
 #include <stdint.h>
+
+#define SECTOR_SIZE 512 // Byte
+
 // in: route
 // directory entry
 // cluster list
@@ -11,10 +14,14 @@
 
 
 // reserve + alloc
-uint32_t read(HANDLE disk, int from, LONG offset, int byte_n);
 
 int main() {
-
+    
+    // uint16_t byte_per_sector = 0;
+    uint8_t sector_per_cluster = 0;
+    uint16_t reserved_sector_n = 0;
+    uint8_t fat_n = 0;
+    uint32_t fat_size = 0;
 
     HANDLE disk = CreateFile("\\\\.\\F:", GENERIC_READ, FILE_SHARE_READ, NULL, OPEN_EXISTING, 0, NULL);
     if (disk == INVALID_HANDLE_VALUE) 
@@ -23,11 +30,6 @@ int main() {
         exit(1);
     }
 
-    uint16_t byte_per_sector = 0;
-    uint8_t sector_per_cluster = 0;
-    uint16_t reserved_sector_n = 0;
-    uint8_t fat_n = 0;
-    uint32_t fat_size = 0;
 
     if (SetFilePointer(disk, 0x0B, NULL, FILE_BEGIN) == INVALID_SET_FILE_POINTER)
     {
@@ -35,7 +37,6 @@ int main() {
         exit(1);
     }
 
-    byte_per_sector = read(disk, FILE_BEGIN, (LONG)0x0B, 2);
     // sector_per_cluster = read(disk, FILE_CURRENT, 0, 1);
     // reserved_sector_n = read(disk, FILE_CURRENT, 0, 2); // in sector
     // fat_n = read(disk, FILE_CURRENT, 0, 1);
@@ -49,48 +50,24 @@ int main() {
     printf("%d\n", data_offset);
     system("PAUSE");
 
-
-    // // ==
-    // SetFilePointer(disk, data_offset, NULL, FILE_BEGIN);
-    // unsigned char buffer[1024];
-    // DWORD bytesRead;
-    // if (!ReadFile(disk, buffer, sizeof(buffer), &bytesRead, NULL)) {
-    //     printf("Failed to read from the disk.\n");
-    //     CloseHandle(disk);
-    //     return 1;
-    // }
-    // for (DWORD i = 0; i < bytesRead; i++) {
-    //     printf("%02X ", buffer[i]);
-    // }
-    // // ==
-    
-
-
     CloseHandle(disk);
     return 0;
 }
 
-uint32_t read(HANDLE disk, int from, LONG offset, int byte_n)
+uint32_t read(BYTE buffer[SECTOR_SIZE], HANDLE disk, int from, int sector_offset/*from 0*/)
 {
-    BYTE buffer = 0;
+    LONG offset = sector_offset * SECTOR_SIZE;
+    BYTE buffer[SECTOR_SIZE] = 0;
     DWORD bytesRead = 0;
-    uint32_t result = 0;
+
     if (SetFilePointer(disk, offset, NULL, from) == INVALID_SET_FILE_POINTER)
     {
         puts("Err when set pointer");
         exit(1);
     }
-    for (int i = 0; i < byte_n; i++)
-    {
-        if (!ReadFile(disk, buffer, sizeof(buffer), &bytesRead, NULL)) {
-            printf("Failed to read from the disk.\n");
-            CloseHandle(disk);
-            exit(1);
-        }
-        else
-        {
-            result += buffer << (i * sizeof(buffer));
-        }
+    if (!ReadFile(disk, buffer, sizeof(buffer), &bytesRead, NULL)) {
+        printf("Failed to read from the disk.\n");
+        CloseHandle(disk);
+        exit(1);
     }
-    return result;
 }
