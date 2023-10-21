1f3bcfbf8a77161dc1c576eed2690c12d4434345
flyingSpagetiMonstr <2021302181087@whu.edu.cn>
2023-10-21 13:06:42 +0800

    N

--- PE/payload.c
+++ PE/payload.c
@@ -1,1 +1,24 @@
-#pragma code_seg("")
+#pragma code_seg("XXX XXX")
+
+#include <windows.h>
+#include <stdio.h>
+
+void fun()
+// int main() {
+//     HMODULE hDLL = LoadLibrary("user32.dll"); // Load the user32.dll library as an example
+
+//     if (hDLL != NULL) {
+//         // Function pointer to the MessageBoxA function in user32.dll
+//         int (*MessageBoxA)(HWND, LPCSTR, LPCSTR, UINT);
+//         MessageBoxA = (int (*)(HWND, LPCSTR, LPCSTR, UINT))GetProcAddress(hDLL, "MessageBoxA");
+
+//         if (MessageBoxA != NULL) {
+//             // Use the function pointer to call MessageBoxA
+//             MessageBoxA(NULL, "Hallo!", "Message", MB_OK);
+//         }
+
+//         FreeLibrary(hDLL); // Unload the library
+//     }
+
+//     return 0;
+// }
